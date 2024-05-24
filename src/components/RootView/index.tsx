8d8798934c7caf5f3a238336b27e7732b14b0cb6
abadaev <username@users.noreply.github.com>
2024-05-24 13:14:00 +0300

    removed unused code and dependencies

--- src/components/RootView/index.tsx
+++ src/components/RootView/index.tsx
@@ -4,19 +4,16 @@ import {COLORS} from '../../constants/colors.ts';
 import {StyleSheet} from 'react-native';
 import {Router} from '../../navigation';
 import {Provider} from 'react-redux';
-import {PersistGate} from 'redux-persist/integration/react';
-import {persistor, store} from '../../store/configureStore.ts';
+import {store} from '../../store/configureStore.ts';
 
 export function RootView() {
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <SafeAreaProvider>
-          <SafeAreaView edges={['top']} style={styles.container}>
-            <Router />
-          </SafeAreaView>
-        </SafeAreaProvider>
-      </PersistGate>
+      <SafeAreaProvider>
+        <SafeAreaView edges={['top']} style={styles.container}>
+          <Router />
+        </SafeAreaView>
+      </SafeAreaProvider>
     </Provider>
   );
 }
--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,44 +1,22 @@
 import {configureStore} from '@reduxjs/toolkit';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import {
-  FLUSH,
-  PAUSE,
-  PERSIST,
-  persistReducer,
-  persistStore,
-  PURGE,
-  REGISTER,
-  REHYDRATE,
-} from 'redux-persist';
 import createSagaMiddleware from 'redux-saga';
 
 import {rootReducer} from './rootReducer';
 import {rootSaga} from './rootSaga';
 
-const persistConfig = {
-  key: 'root',
-  storage: AsyncStorage,
-  whitelist: [],
-};
-
 const sagaMiddleware = createSagaMiddleware();
 
 const middlewares = [sagaMiddleware];
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: rootReducer,
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
       serializableCheck: {
         ignoredActionPaths: [],
         ignoredPaths: [],
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
     }).concat(middlewares),
 });
-
-export const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
